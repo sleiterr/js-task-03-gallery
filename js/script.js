55403c5707922bbf3a37396b4a308e03ad55b4f3
Oleg <sleite4r@gmail.com>
2025-01-21 18:13:32 +0100

    create js, add gallery use for Each

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const outputGallery = document.querySelector(".output gallery");
+
+const galleryImages = [
+  {
+    src: "https://picsum.photos/300/300?random=1",
+    title: "Billedoverskrift 1",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=2",
+    title: "Billedoverskrift 2",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=3",
+    title: "Billedoverskrift 3",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=4",
+    title: "Billedoverskrift 4",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=5",
+    title: "Billedoverskrift 5",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=6",
+    title: "Billedoverskrift 6",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=7",
+    title: "Billedoverskrift 7",
+    caption: "Beskrivelse...",
+  },
+  {
+    src: "https://picsum.photos/300/300?random=8",
+    title: "Billedoverskrift 8",
+    caption: "Beskrivelse...",
+  },
+];
+
+galleryImages.forEach((item) => {
+  outputGallery.innerHTML += `
+    <img src=${item.src}><>
+    `;
+});
